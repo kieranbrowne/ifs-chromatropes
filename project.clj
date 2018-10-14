1c81c32305af44f36fbf8351ad77b1f01e2ab652
Kieran Browne <kierantbrowne@gmail.com>
2018-10-15 00:11:23 +1100

    removed the need for overtone

--- project.clj
+++ project.clj
@@ -4,16 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [overtone  "0.9.1" :exclusions  [[clj-native]]]
-                 [shadertone "0.2.5"]
-                 [clj-native "0.9.5"]
-                 ])
-; (defproject tonetest "0.1.0-SNAPSHOT"
-;   :description "FIXME: write description"
-;   :url "http://example.com/FIXME"
-;   :license {:name "Eclipse Public License"
-;             :url "http://www.eclipse.org/legal/epl-v10.html"}
-;   :dependencies [[org.clojure/clojure "1.8.0"]
-;                  [overtone  "0.9.1" :exclusions  [[clj-native]]]
-;                  [shadertone "0.2.5"]
-;                  [clj-native  "0.9.5"]])
+                 [shadertone "0.2.5"]])
